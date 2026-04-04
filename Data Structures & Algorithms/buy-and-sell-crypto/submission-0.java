2dbdfb0a0871f72cfd4504ee9f9631785977996f
Ajay Kumar <abharoja@gmail.com>
2026-04-04 14:01:24 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length < 2){
+            return 0;
+        }
+
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] < buyPrice){
+                buyPrice = prices[i];
+            }else{
+                maxProfit = Math.max(maxProfit, prices[i] - buyPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
