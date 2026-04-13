4d3f8372736f1ea2040d1bfc31f2d8db277c0001
Ajay Kumar <abharoja@gmail.com>
2026-04-13 12:08:04 +0530

    Add: maximum-frequency-stack - submission-0

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-0.java
@@ -0,0 +1,33 @@
+class FreqStack {
+    private Map<Integer, Integer> freqMap;
+    private Map<Integer, Stack<Integer>> groupMap;
+    private int maxFreq;
+
+    public FreqStack() {
+        freqMap = new HashMap<>();
+        groupMap = new HashMap<>();
+        maxFreq = 0;
+    }
+    
+    public void push(int val) {
+        int freq = freqMap.getOrDefault(val, 0) + 1;
+        freqMap.put(val, freq);
+
+        maxFreq = Math.max(maxFreq, freq);
+
+        groupMap.putIfAbsent(freq, new Stack<>());
+        groupMap.get(freq).push(val);
+    }
+    
+    public int pop() {
+        int val = groupMap.get(maxFreq).pop();
+        
+        freqMap.put(val, freqMap.get(val) - 1);
+
+        if (groupMap.get(maxFreq).isEmpty()) {
+            maxFreq--;
+        }
+
+        return val;
+    }
+}
